ec6882eb9da6569c3fdae97528f5049ba6d87c79
Simon <gevhaz@tutanota.com>
2022-06-23 00:19:33 +0200

    Use more informative variable names

--- seasonwatch/app.py
+++ seasonwatch/app.py
@@ -26,63 +26,61 @@ def main():
     notify2.init("seasonwatch")
 
     for show_cfg in config['series']:
-        current_season_cfg: int = show_cfg.get('current_season')
-        title_cfg: str = show_cfg.get('title')
-        id_cfg: str = show_cfg.get('id')
-        if current_season_cfg is None or not title_cfg or not id_cfg:
+        last_watched_season: int = show_cfg.get('current_season')
+        series_name: str = show_cfg.get('title')
+        series_id: str = show_cfg.get('id')
+
+        if last_watched_season is None or series_name is None or series_id is None:
             print("Data is missing from the config. Skipping this show")
             continue
 
-        show_imdb = ia.get_movie(id_cfg, 'episodes')
-        episodes_imdb = show_imdb.data.get("episodes")
+        imdb_all_seasons_data = ia.get_movie(series_id, 'episodes').data.get("episodes")
 
-        if episodes_imdb:
-            n_latest_season: int = max(episodes_imdb.keys())
-        else:
-            print(f"Couldn't load episodes for {title_cfg}. Skipping...")
+        if imdb_all_seasons_data is None:
+            print(f"Couldn't load episodes for {series_name}. Skipping...")
             continue
 
+        latest_available_season: int = max(imdb_all_seasons_data.keys())
 
-        if n_latest_season <= current_season_cfg:
-            print(f"No new season found for {title_cfg}")
+        if latest_available_season <= last_watched_season:
+            print(f"No new season found for {series_name}")
             continue
 
-        next_season = current_season_cfg + 1
-        release_dates_raw = ia.get_movie_release_dates(episodes_imdb[next_season][1].getID())
+        next_unwatched_season = last_watched_season + 1
+        imdb_release_data = ia.get_movie_release_dates(imdb_all_seasons_data[next_unwatched_season][1].getID()).get("data")
 
-        release_data_imdb = release_dates_raw.get('data')
         # There is no data about the release date of the new season
-        if release_data_imdb is None:
-            print(f"Season {next_season} of {title_cfg} is announced but "
+        if imdb_release_data is None:
+            print(f"Season {next_unwatched_season} of {series_name} is announced but "
                   "the release date is not yet determined")
 
         # Parse date from release dates of all countries and take the minimum
-        raw_release_dates_imdb = release_data_imdb.get('raw release dates')
-        if raw_release_dates_imdb is None:
+        imdb_raw_release_dates = imdb_release_data.get('raw release dates')
+        if imdb_raw_release_dates is None:
             continue
 
         try:
-            earliest_date: datetime = min(parse(d['date'], default=datetime(2022, 12, 31)) for d in raw_release_dates_imdb)
+            next_season_earliest_release: datetime = min(parse(d['date'], default=datetime(2022, 12, 31)) for d in imdb_raw_release_dates)
         except ParserError as e:
-            print(Fore.RED + f"There was an error parsing a date value for the show {title_cfg}")
+            print(Fore.RED + f"There was an error parsing a date value for the show {series_name}")
             print(e)
             continue
 
         # The new season is out
-        if (earliest_date < datetime.now()):
-            print(Fore.BLUE + f"Season {next_season} of {title_cfg} is out already!")
-            n = notify2.Notification(title_cfg, f"Season {next_season} of {title_cfg} is out already!")
+        if (next_season_earliest_release < datetime.now()):
+            print(Fore.BLUE + f"Season {next_unwatched_season} of {series_name} is out already!")
+            n = notify2.Notification(series_name, f"Season {next_unwatched_season} of {series_name} is out already!")
             n.show()
         # The new season is not yet out
-        elif (earliest_date < datetime.now() + timedelta(days=90)):
-            print(Fore.GREEN + f"Season {next_season} of {title_cfg} is not yet out but will "
-                  f"be released on {earliest_date.strftime('%B %-d, %Y')}.")
-            n = notify2.Notification(title_cfg,
-                    f"Season {next_season} of {title_cfg} is not yet out but will "
-                    f"be released on {earliest_date.strftime('%B %-d, %Y')}.")
+        elif (next_season_earliest_release < datetime.now() + timedelta(days=90)):
+            print(Fore.GREEN + f"Season {next_unwatched_season} of {series_name} is not yet out but will "
+                  f"be released on {next_season_earliest_release.strftime('%B %-d, %Y')}.")
+            n = notify2.Notification(series_name,
+                    f"Season {next_unwatched_season} of {series_name} is not yet out but will "
+                    f"be released on {next_season_earliest_release.strftime('%B %-d, %Y')}.")
             n.show()
         else:
-            print(f"Season {next_season} of {title_cfg} coming up, in more than three months")
+            print(f"Season {next_unwatched_season} of {series_name} coming up, in more than three months")
 
 
 if __name__ == "__main__":
