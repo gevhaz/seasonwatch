25ff9ad1e1b11a179bede0c2cd4a85eb769a8dd0
Simon <gevhaz@tutanota.com>
2022-06-23 17:34:58 +0200

    Move getting episode id to function
    The mechanism to get the id of the first episode in the upcoming season
    is fairly standalone, so it has been moved to a separate function.

--- seasonwatch/app.py
+++ seasonwatch/app.py
@@ -5,75 +5,72 @@ from datetime import datetime, timedelta
 import imdb
 import notify2
 from colorama import Fore, init
-from dateutil.parser import ParserError, parse
 from imdb.parser.http import IMDbHTTPAccessSystem
 from imdb.parser.s3 import IMDbS3AccessSystem
 from imdb.parser.sql import IMDbSqlAccessSystem
 
 from seasonwatch.config import ConfigParser
-from seasonwatch.exceptions import ConfigException
+from seasonwatch.exceptions import ConfigException, SeasonwatchException
+from seasonwatch.utils import Utils
 
 init(autoreset=True)
 
 
-def check_for_new_seasons(series_config: dict[str, str], ia) -> tuple[str, str]:
-    last_watched_season: int = int(series_config["current_season"])
-    series_name: str = series_config["title"]
-    series_id: str = series_config["id"]
+def get_next_episode_id(id: str, last_watched_season: int, ia) -> str | None:
+    next_season = last_watched_season + 1
 
-    imdb_all_seasons_data = ia.get_movie(series_id, "episodes").data.get("episodes")
+    imdb_all_seasons_data = ia.get_movie(id, "episodes").data.get("episodes")
 
     if imdb_all_seasons_data is None:
-        return Fore.YELLOW, f"Couldn't load episodes for {series_name}. Skipping..."
+        raise SeasonwatchException("Couldn't load episodes. Skipping...")
 
     latest_available_season: int = max(imdb_all_seasons_data.keys())
 
     if latest_available_season <= last_watched_season:
-        return Fore.RESET, f"No new season found for {series_name}"
+        return None
 
-    next_unwatched_season = last_watched_season + 1
-    imdb_release_data = ia.get_movie_release_dates(
-        imdb_all_seasons_data[next_unwatched_season][1].getID()
-    ).get("data")
+    return imdb_all_seasons_data[next_season][1].getID()
 
-    # There is no data about the release date of the new season
-    if imdb_release_data is None:
-        print(
-            f"Season {next_unwatched_season} of {series_name} is announced but "
-            "the release date is not yet determined"
-        )
 
-    # Parse date from release dates of all countries and take the minimum
-    imdb_raw_release_dates = imdb_release_data.get("raw release dates")
-    if imdb_raw_release_dates is None:
-        return Fore.YELLOW, f"Couldn't find IMDb release dates for {series_name}"
+def check_for_new_seasons(series_config: dict[str, str], ia) -> tuple[str, str]:
+    last_watched_season = int(series_config["current_season"])
+    next_season = last_watched_season + 1
+    name = series_config["title"]
+    id = series_config["id"]
 
     try:
-        next_season_earliest_release: datetime = min(
-            parse(d["date"], default=datetime(2022, 12, 31))
-            for d in imdb_raw_release_dates
+        next_episode_id = get_next_episode_id(id, last_watched_season, ia)
+    except SeasonwatchException as e:
+        return Fore.RED, f"{name}: {str(e)}"
+
+    if next_episode_id is None:
+        return Fore.RESET, f"No new season found for {name}"
+
+    try:
+        next_season_earliest_release = Utils.get_next_release_date(
+            name,
+            next_season,
+            next_episode_id,
+            ia,
         )
-    except ParserError as e:
-        raise Exception(
-            f"There was an error parsing date value for the show {series_name}: {e}"
-        )
+    except SeasonwatchException as e:
+        return Fore.RED, str(e)
 
     # The new season is out
     if next_season_earliest_release < datetime.now():
-        message = f"Season {next_unwatched_season} of {series_name} is out already!"
+        message = f"Season {next_season} of {name} is out already!"
         return Fore.BLUE, message
     # The new season is not yet out
     elif next_season_earliest_release < datetime.now() + timedelta(days=90):
         message = (
-            f"Season {next_unwatched_season} of {series_name} is not yet out but "
+            f"Season {next_season} of {name} is not yet out but "
             "will be released on "
             f"{next_season_earliest_release.strftime('%B %-d, %Y')}."
         )
         return Fore.GREEN, message
     else:
         message = (
-            f"Season {next_unwatched_season} of {series_name} coming up, in more "
-            "than three months"
+            f"Season {next_season} of {name} coming up, in more " "than three months"
         )
         return Fore.RESET, message
 
