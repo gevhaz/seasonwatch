f86da031a15eb7d6b48c237d431ed166583f5adb
Simon <gevhaz@tutanota.com>
2022-08-04 01:20:16 +0200

    Set discogs token through CLI
    Ability to set the discogs API token through the configure verb has been
    added. The key is saved to ~/.config/seasonwatch/config, which is an ini
    file.

--- seasonwatch/constants.py
+++ seasonwatch/constants.py
@@ -3,6 +3,9 @@ from pathlib import Path
 
 class Constants:
     DATA_DIRECTORY = Path.home() / ".seasonwatch"
+    CONFIG_DIRECTORY = Path.home() / ".config" / "seasonwatch"
+    CONFIG_FILE = "config"
+    CONFIG_PATH = CONFIG_DIRECTORY / CONFIG_FILE
     DATABASE_FILE = "database.sqlite"
     DATABASE_PATH = str(DATA_DIRECTORY / DATABASE_FILE)
 
