ac73f35219e71eb98f171ee4392d38b36db9f621
Simon <gevhaz@tutanota.com>
2023-11-03 01:42:13 +0100

    Use XDG base directories for config and data

--- seasonwatch/constants.py
+++ seasonwatch/constants.py
@@ -1,10 +1,21 @@
+import os
 from pathlib import Path
 
 
 class Constants:
-    DATA_DIRECTORY = Path.home() / ".seasonwatch"
-    CONFIG_DIRECTORY = Path.home() / ".config" / "seasonwatch"
-    CONFIG_FILE = "config"
+    DATA_DIRECTORY = Path(
+        os.environ.get(
+            "XDG_DATA_HOME",
+            default=Path.home() / ".local" / "share" / "seasonwatch",
+        )
+    )
+    CONFIG_DIRECTORY = Path(
+        os.environ.get(
+            "XDG_CONFIG_HOME",
+            default=Path.home() / ".config",
+        )
+    )
+    CONFIG_FILE = "seasonwatchrc"
     CONFIG_PATH = CONFIG_DIRECTORY / CONFIG_FILE
     DATABASE_FILE = "database.sqlite"
     DATABASE_PATH = str(DATA_DIRECTORY / DATABASE_FILE)
